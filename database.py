8163a3b9ce07f2dc34ac14a50a254c582383c716
Andra Ditya <radityairawan259@gmail.com>
2026-06-19 08:36:11 +0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from supabase import create_client, Client
+
+# Inisialisasi klien Supabase menggunakan secrets
+url: str = st.secrets["SUPABASE_URL"]
+key: str = st.secrets["SUPABASE_KEY"]
+supabase: Client = create_client(url, key)
+
+def login_user(email, password):
+    try:
+        res = supabase.auth.sign_in_with_password({"email": email, "password": password})
+        return res.user
+    except Exception as e:
+        st.error(f"⚠️ Gagal Masuk: {e}")
+        return None
+
+def register_user(email, password):
+    try:
+        res = supabase.auth.sign_up({"email": email, "password": password})
+        return res.user
+    except Exception as e:
+        st.error(f"⚠️ Gagal Mendaftar: {e}")
+        return None
+
+def logout_user():
+    supabase.auth.sign_out()
+
+def get_all_journals():
+    response = supabase.table("journals").select("*").order("created_at", desc=True).execute()
+    return response.data
+
+def insert_journal(user_id, user_email, title, content):
+    data = {
+        "user_id": user_id,
+        "user_email": user_email,
+        "title": title,
+        "content": content
+    }
+    response = supabase.table("journals").insert(data).execute()
+    return response.data
